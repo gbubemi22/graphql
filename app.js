4874bfa12b018f422af09255a38c66bb1b71e447
gbubemi22 <slimnfine22@gmail.com>
2023-05-13 14:33:05 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const graphqlHttp = require('express-graphql');
+const { buildSchema } = require('graphql');
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
+
+
+app.use('/graphql', require('express-graphql').graphqlHTTP({
+    schema: buildSchema(`
+        type   RootQuery  {
+            events: [String!]!
+        }
+        type RootMutation {
+             createEvent(name: String): String
+        }
+
+        schema {
+          query: RootQuery
+          mutation:RootMutation
+        }
+    `),
+    rootValue: {
+       events: () => {
+          return ['Romatic Cooking', 'Sailing', 'All-Night Codeing']
+       },
+       createEvent: (args) => { 
+         const eventName = args.name;
+         return eventName
+       }
+    },
+    graphiql: true
+}));
+
+
+
+app.listen(3000) 
